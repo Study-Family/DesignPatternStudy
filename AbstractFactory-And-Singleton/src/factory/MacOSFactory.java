c1e8be93126f58e60bee44a25f85981d1c3fb2f5
wonu606 <wonu606@gmail.com>
2023-06-22 14:49:22 +0900

    chore: 맥 팩토리명이 잘못되어 있어 수정

--- AbstractFactory-And-Singleton/src/factory/MacOSFactory.java
+++ AbstractFactory-And-Singleton/src/factory/MacOSFactory.java
@@ -5,7 +5,7 @@ import checkbox.Checkbox;
 import button.Button;
 import checkbox.MacOSCheckbox;
 
-public class MaxOSFactory implements GUIFactory {
+public class MacOSFactory implements GUIFactory {
 
     @Override
     public Button createButton() {
