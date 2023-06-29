79de5f2c5fac28ae2b73dabd166b1b96317fd5b5
bombo-dev <bombo96@chungwoon.ac.kr>
2023-06-30 01:01:55 +0900

    feat : 사각 못을 둥근 못으로 바꾸는 어댑터 클래스 구현

---
+++ chapter7/adapter/src/main/java/domain/SquarePeg.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class SquarePeg {
+    private int width;
+
+    public SquarePeg(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
